d9f2fd21fe9b2e1cb161caabeed6d70c4749d9fc
Luis_Eduardo <undernet1@gmail.com>
2023-04-15 13:10:31 -0400

    Added login screen

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,3 +1,25 @@
+import './login.css';
+
 export const Login = () => {
-  return <>Login</>;
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-body">
+          <div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Username"
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </div>
+    </div>
+  );
 };
