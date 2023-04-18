57769c0757264aa5677acf63ee3b950cd112c7bc
Luis_Eduardo <undernet1@gmail.com>
2023-04-18 19:56:04 -0400

    Working on a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Router } from './router/Router';
 import { UserProvider } from './context/UserContext';
+import { Navbar } from './components/sidenav/Navbar';
 
 export const App = () => {
   return (
     <div className="App">
       <UserProvider>
+        <Navbar />
         <main>
           <Router />
         </main>
