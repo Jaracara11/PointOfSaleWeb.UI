d347076e15f17ebebddcf70d1cb1141286b133a1
Luis_Eduardo <undernet1@gmail.com>
2023-04-17 20:00:41 -0400

    changed userURL var name

--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { UserLogin } from '../interfaces/UserLogin';
 
-const userURL = import.meta.env.VITE_API_URL + '/api/user';
+const USER_URL = import.meta.env.VITE_API_URL + '/api/user';
 
 export const login = async (userData: UserLogin): Promise<UserInfo> => {
   try {
-    const response = await axios.post(`${userURL}/login`, userData);
+    const response = await axios.post(`${USER_URL}/login`, userData);
     return response.data;
   } catch (error: any) {
     return Promise.reject(error.response.data);
