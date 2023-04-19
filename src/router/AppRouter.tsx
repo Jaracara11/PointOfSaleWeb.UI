6f301cb2f414fde2441a573b5ef46f536019887e
Luis_Eduardo <undernet1@gmail.com>
2023-04-19 14:05:15 -0400

    added aditional routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -10,6 +10,8 @@ import { Inventory } from '../pages/inventory/Inventory';
 import { NotFound } from '../pages/notFound/NotFound';
 import { PrivateRoutes } from './PrivateRoutes';
 import { RootLayout } from '../layouts/RootLayout';
+import { Categories } from '../pages/categories/Categories';
+import { Products } from '../pages/products/Products';
 
 export const AppRouter = () => {
   const router = createBrowserRouter(
@@ -20,7 +22,12 @@ export const AppRouter = () => {
         <Route element={<RootLayout />}>
           <Route element={<PrivateRoutes />}>
             <Route path="/home" element={<Home />} />
-            <Route path="/inventory" element={<Inventory />} />
+            <Route element={<Inventory />} />
+            <Route path="/inventory">
+              <Route index element={<Inventory />} />
+              <Route path="categories" element={<Categories />} />
+              <Route path="products" element={<Products />} />
+            </Route>
           </Route>
         </Route>
       </Route>
