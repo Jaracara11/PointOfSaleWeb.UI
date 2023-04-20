afe1ebee164862c52e2fb00c82f7d3bd1ee85bfc
Luis_Eduardo <undernet1@gmail.com>
2023-04-20 17:29:43 -0400

    tryin to fix the layout

--- src/components/sidebarMenu/SidebarMenu.tsx
+++ src/components/sidebarMenu/SidebarMenu.tsx
@@ -7,40 +7,36 @@ export const SidebarMenu = () => {
   //console.log(user);
   return (
     user && (
-      <div className="container-fluid" id="sidebar-menu">
-        <div className="row">
-          <div className="bg-dark col-auto col-md-3 min-vh-100 d-flex">
-            <div>
-              <span>PointOfSale.Web</span>
-              <hr className="text-secondary d-none d-sm-block" />
-              <ul className="nav nav-pills flex-column">
-                <li className="nav-item py-sm-0">
-                  <NavLink className="nav-link fs-5" to="/home">
-                    <i className="bi bi-house"></i>
-                    <span className="ms-2 d-none d-sm-inline">Home</span>
-                  </NavLink>
-                </li>
-                <li className="nav-item py-sm-0">
-                  <NavLink className="nav-link fs-5" to="sales">
-                    <i className="bi bi-cash-coin"></i>
-                    <span className="ms-2 d-none d-sm-inline">Sales</span>
-                  </NavLink>
-                </li>
-                <li className="nav-item py-sm-0">
-                  <NavLink className="nav-link fs-5" to="/products">
-                    <i className="bi bi-box"></i>
-                    <span className="ms-2 d-none d-sm-inline">Products</span>
-                  </NavLink>
-                </li>
-                <li className="nav-item py-sm-0">
-                  <NavLink className="nav-link fs-5" to="inventory">
-                    <i className="bi bi-clipboard-check"></i>
-                    <span className="ms-2 d-none d-sm-inline">Inventory</span>
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-          </div>
+      <div className="bg-dark" id="sidebar-menu">
+        <div>
+          <span>PointOfSale.Web</span>
+          <hr className="text-secondary d-none d-sm-block" />
+          <ul className="nav nav-pills flex-column">
+            <li className="nav-item py-sm-0">
+              <NavLink className="nav-link fs-5" to="/home">
+                <i className="bi bi-house"></i>
+                <span className="ms-2 d-none d-sm-inline">Home</span>
+              </NavLink>
+            </li>
+            <li className="nav-item py-sm-0">
+              <NavLink className="nav-link fs-5" to="sales">
+                <i className="bi bi-cash-coin"></i>
+                <span className="ms-2 d-none d-sm-inline">Sales</span>
+              </NavLink>
+            </li>
+            <li className="nav-item py-sm-0">
+              <NavLink className="nav-link fs-5" to="/products">
+                <i className="bi bi-box"></i>
+                <span className="ms-2 d-none d-sm-inline">Products</span>
+              </NavLink>
+            </li>
+            <li className="nav-item py-sm-0">
+              <NavLink className="nav-link fs-5" to="inventory">
+                <i className="bi bi-clipboard-check"></i>
+                <span className="ms-2 d-none d-sm-inline">Inventory</span>
+              </NavLink>
+            </li>
+          </ul>
         </div>
       </div>
     )
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,13 +1,20 @@
+import './rootLayout.css';
 import { Outlet } from 'react-router-dom';
 import { SidebarMenu } from '../components/sidebarMenu/SidebarMenu';
 
 export const RootLayout = () => {
   return (
     <div className="root-layout">
-      <SidebarMenu />
-      <main className="container">
-        <Outlet />
-      </main>
+      <div className="row">
+        <div className="col-3">
+          <SidebarMenu />
+        </div>
+        <div className="col-9">
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </div>
     </div>
   );
 };
