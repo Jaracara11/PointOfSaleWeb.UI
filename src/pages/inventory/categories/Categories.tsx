0f54f921df1dddb307d09cf224a111fb49754a92
Luis_Eduardo <undernet1@gmail.com>
2023-04-24 16:19:20 -0400

    Working on category table

--- src/pages/inventory/categories/Categories.tsx
+++ src/pages/inventory/categories/Categories.tsx
@@ -1,3 +1,20 @@
 export const Categories = () => {
-  return <>Categories</>;
+  return (
+    <div className="container-fluid">
+      <table className="table">
+        <thead>
+          <tr>
+            <td>Category ID</td>
+            <td>Category Name</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Placeholder</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
