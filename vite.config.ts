fa8f6ede3c45cb28c982e26e9c8be747548ef38e
Luis_Eduardo <undernet1@gmail.com>
2023-05-29 11:41:04 -0400

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    https: {
-      protocol: 'TLSv1.2',
-      key: 'public/key.pem',
-      cert: 'public/cert.pem'
-    }
+    port: 3000
   }
 });
