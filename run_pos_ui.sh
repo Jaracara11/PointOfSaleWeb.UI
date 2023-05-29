dacb8ccc36746e12663abcf9e317124cfe9877e5
Luis_Eduardo <undernet1@gmail.com>
2023-05-29 15:28:21 -0400

    Added command to deploy container in linux

---
+++ run_pos_ui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+cd PointOfSaleWeb.UI
+git checkout master
+git pull
+docker stop pos-web.ui
+docker rm pos-web.ui
+docker build -t pos.ui .
+docker run -d -p 3000:3000 --restart unless-stopped --name pos-web.ui pos.ui
+docker image prune -f
+
+echo "List of running Docker containers:"
+docker ps
