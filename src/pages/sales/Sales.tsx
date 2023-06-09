0f96ef64b147798d8accde3074ec60d38921e581
Luis_Eduardo <undernet1@gmail.com>
2023-06-09 15:36:36 -0400

    Working on the sales component

--- src/pages/sales/Sales.tsx
+++ src/pages/sales/Sales.tsx
@@ -1,17 +1,62 @@
 import './sales.css';
 import { LoadingSpinner } from '../../components/loadingSpinner/LoadingSpinner';
 import { useGetProducts } from '../../hooks/products.hooks';
+import { Table } from 'react-bootstrap';
+import { useState } from 'react';
+import { Product } from '../../interfaces/inventory/product';
+import { SearchInput } from '../../components/searchInput/SearchInput';
+import { useGetCategories } from '../../hooks/categories.hooks';
+import { getProductCategoryName } from '../../utils/inventory.helper';
 
 export const Sales = () => {
   const productsQuery = useGetProducts();
+  const categoriesQuery = useGetCategories();
+  const [searchProductQuery, setSearchProductQuery] = useState<string>('');
 
-  if (productsQuery.isLoading) return <LoadingSpinner />;
+  const filteredProducts = (productsQuery.data || []).filter((product) =>
+    product.productName.toLowerCase().includes(searchProductQuery.trim().toLowerCase())
+  );
 
-  console.log(productsQuery.data);
+  if (productsQuery.isLoading || categoriesQuery.isLoading) return <LoadingSpinner />;
 
   return (
     <div className="sales-container">
       <h1 className="title">Sales</h1>
+      <div className="products-table">
+        <div className="card">
+          <SearchInput searchQuery={searchProductQuery} setSearchQuery={setSearchProductQuery} />
+          {productsQuery.data && (
+            <>
+              <Table bordered hover>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Stock</th>
+                    <th>Price</th>
+                    <th>Category</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {filteredProducts.map((product: Product) => (
+                    <tr key={product.productID}>
+                      <td>
+                        <i className="bi bi-dot"></i>
+                        {product.productName}
+                      </td>
+                      <td>{product.productStock}</td>
+                      <td>{product.productPrice}</td>
+                      <td>
+                        {categoriesQuery.data &&
+                          getProductCategoryName(product.productCategoryID, categoriesQuery.data)}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
