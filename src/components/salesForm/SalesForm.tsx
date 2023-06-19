1068bedfda02bd68a139e17f45be90a0fbd9ec2e
Luis_Eduardo <undernet1@gmail.com>
2023-06-19 14:21:46 -0400

    .

--- src/components/salesForm/SalesForm.tsx
+++ src/components/salesForm/SalesForm.tsx
@@ -3,7 +3,7 @@ import { Button, Table } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import { Product } from '../../interfaces/inventory/product';
 import { SalesFormProps } from '../../interfaces/SalesFormProps';
-import { swalMessageAlert } from '../../services/swal.service';
+import { swalConfirmAlert, swalMessageAlert } from '../../services/swal.service';
 import { UserAuth } from '../../context/UserContext';
 import { useGetDiscountsByUser } from '../../hooks/sales.hooks';
 import { LoadingSpinner } from '../loadingSpinner/LoadingSpinner';
@@ -96,11 +96,34 @@ export const SalesForm = ({ products, removeFromCart }: SalesFormProps) => {
   const handleDiscountChange = (event: React.ChangeEvent<HTMLSelectElement>) =>
     setDiscount(parseFloat(event.target.value));
 
+  const clearCart = async () => {
+    let confirmTitle = 'Are you sure you want to clear the cart?';
+
+    const isConfirmed = await swalConfirmAlert(confirmTitle, 'Clear', 'warning');
+
+    if (isConfirmed) {
+      productSales.forEach((product) => removeFromCart(product.productID || 0));
+
+      setProductSales([]);
+
+      setProductSales((prevProductSales) =>
+        prevProductSales.map((product) => ({
+          ...product,
+          productQuantity: 0
+        }))
+      );
+
+      setSubtotal(0);
+      setTotal(0);
+      setDiscount(0);
+    }
+  };
+
   if (discountsQuery.isLoading) return <LoadingSpinner />;
 
   return (
     <div className="sales-form card bg-light">
-      <h4>Current Order</h4>
+      <h4 className="title">Current Order</h4>
       <Table hover>
         <thead>
           <tr>
@@ -122,52 +145,67 @@ export const SalesForm = ({ products, removeFromCart }: SalesFormProps) => {
               <td>{product.productPrice}</td>
               <td>{product.productQuantity}</td>
               <td>
-                <div className="quantity-control">
-                  <Button
-                    variant="dark"
-                    size="sm"
-                    onClick={() => handleIncreaseQuantity(product.productID || 0)}
-                  >
-                    <i className="bi bi-plus"></i>
-                  </Button>
-                  <Button
-                    variant="dark"
-                    size="sm"
-                    onClick={() => handleDecreaseQuantity(product.productID || 0)}
-                  >
-                    <i className="bi bi-dash"></i>
-                  </Button>
-                </div>
+                <Button
+                  variant="dark"
+                  size="sm"
+                  onClick={() => handleIncreaseQuantity(product.productID || 0)}
+                >
+                  <i className="bi bi-plus"></i>
+                </Button>
+                <Button
+                  variant="dark"
+                  size="sm"
+                  onClick={() => handleDecreaseQuantity(product.productID || 0)}
+                >
+                  <i className="bi bi-dash"></i>
+                </Button>
               </td>
             </tr>
           ))}
         </tbody>
       </Table>
 
-      <div className="order-info">
-        <span>
-          <strong>Sub-total:</strong> {calculateSubTotal().toFixed(2)}
-        </span>
-        <span>
-          <strong>Discount:</strong>
-          <select
-            name="order-discount"
-            value={discount}
+      <div>
+        <div>
+          <ul>
+            <li>
+              <strong>Sub-total:</strong> {calculateSubTotal().toFixed(2)}
+            </li>
+            <li>
+              <strong>Discount:&nbsp; </strong>
+              <select
+                name="order-discount"
+                value={discount}
+                disabled={productSales.length === 0}
+                onChange={handleDiscountChange}
+              >
+                <option value={0}>0%</option>
+                {discountsQuery.data &&
+                  discountsQuery.data.map((discount) => (
+                    <option key={discount} value={discount}>
+                      {discount * 100}%
+                    </option>
+                  ))}
+              </select>
+            </li>
+            <li>
+              <strong>Total:</strong> {total.toFixed(2)}
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <Button variant="dark" disabled={productSales.length === 0}>
+            <i className="bi bi-coin"></i>&nbsp; Check Out
+          </Button>
+          <Button
+            variant="outline-dark"
+            onClick={() => clearCart()}
             disabled={productSales.length === 0}
-            onChange={handleDiscountChange}
           >
-            <option value={0}>0%</option>
-            {discountsQuery.data &&
-              discountsQuery.data.map((discount) => (
-                <option key={discount} value={discount}>
-                  {discount * 100}%
-                </option>
-              ))}
-          </select>
-        </span>
-        <span>
-          <strong>Total:</strong> {total.toFixed(2)}
-        </span>
+            <i className="bi bi-eraser"></i>&nbsp; Clear Cart
+          </Button>
+        </div>
       </div>
     </div>
   );
