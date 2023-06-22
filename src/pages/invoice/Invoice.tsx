11f2642d8299fe81e2a54bec58395ebac62a9878
Luis_Eduardo <undernet1@gmail.com>
2023-06-22 09:04:49 -0400

    Working on invoice table

--- src/pages/invoice/Invoice.tsx
+++ src/pages/invoice/Invoice.tsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { NotFound } from '../notFound/NotFound';
 import { Order } from '../../interfaces/order/Order';
 import { Table } from 'react-bootstrap';
+import { Product } from '../../interfaces/inventory/product';
 
 export const Invoice = () => {
   const location = useLocation();
@@ -35,6 +36,27 @@ export const Invoice = () => {
                   <th>Total</th>
                 </tr>
               </thead>
+              <tbody>
+                {orderInfo.products.map((orderItem: Product, index: number) => {
+                  const cumulativeTotal = orderInfo.products
+                    .slice(0, index + 1)
+                    .reduce(
+                      (total, item) =>
+                        total + (item.productPrice || 0) * (item.productQuantity || 1),
+                      0
+                    );
+
+                  return (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td>{orderItem.productName}</td>
+                      <td>{orderItem.productPrice}</td>
+                      <td>{orderItem.productQuantity}</td>
+                      <td>{cumulativeTotal}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
             </Table>
           </div>
         </div>
