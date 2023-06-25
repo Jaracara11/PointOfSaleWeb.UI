6c5cf551de5cc4e7226c02c37fd464e31f21d685
Luis_Eduardo <undernet1@gmail.com>
2023-06-25 15:47:41 -0400

    fixed inconsistency on imports

--- src/repository/saleRepository.ts
+++ src/repository/saleRepository.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { userAuthorizationHeaders } from '../services/user.Service';
-import { Order } from '../interfaces/order/OrderRequest';
+import { OrderInfo } from '../interfaces/order/OrderInfo';
+import { OrderRequest } from '../interfaces/order/OrderRequest';
 
 const API_URL = import.meta.env.VITE_API_URL + '/sale';
 
@@ -15,12 +16,12 @@ export const GetAvailableDiscounts = async (username: string): Promise<number[]>
   }
 };
 
-export const checkoutOrder = async (order: Order): Promise<Order> => {
+export const checkoutOrder = async (order: OrderRequest): Promise<OrderInfo> => {
   try {
     const response = await axios.post(`${API_URL}/checkout-order`, order, {
       headers: userAuthorizationHeaders()
     });
-    return response.data as Order;
+    return response.data as OrderInfo;
   } catch (error: any) {
     return Promise.reject(error);
   }
