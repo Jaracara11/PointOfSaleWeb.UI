ad727c2cbca49b2b3d5e52e11bb510d7749961f4
Luis_Eduardo <undernet1@gmail.com>
2023-07-04 12:25:12 -0400

    Added datetime pickers

--- src/pages/sales/Sales.tsx
+++ src/pages/sales/Sales.tsx
@@ -1,3 +1,53 @@
+import './sales.css';
+import { useState } from 'react';
+import { Button, Table } from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
 export const Sales = () => {
-  return <>Sales</>;
+  const [initialDate, setInitialDate] = useState<Date | null>(null);
+  const [finalDate, setFinalDate] = useState<Date | null>(null);
+
+  return (
+    <div className="sales">
+      <div className="row">
+        <h1 className="title">Sales</h1>
+      </div>
+      <div className="row">
+        <div className="date-pickers">
+          <div>
+            <span className="text-muted">Order Date Between: </span>
+
+            <DatePicker
+              enableTabLoop={false}
+              selected={initialDate}
+              onChange={(date) => setInitialDate(date as Date)}
+              className="form-control"
+            />
+
+            <DatePicker
+              enableTabLoop={false}
+              selected={finalDate}
+              onChange={(date) => setFinalDate(date as Date)}
+              className="form-control"
+            />
+          </div>
+
+          <div>
+            <Button variant="dark">Search</Button>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <Table hover>
+          <thead>
+            <tr>
+              <th>Invoice ID</th>
+            </tr>
+          </thead>
+        </Table>
+      </div>
+    </div>
+  );
 };
