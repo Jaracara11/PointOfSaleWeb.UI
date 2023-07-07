5bc8a830ba88f04909ff383234d20fa10ae8654e
Luis_Eduardo <undernet1@gmail.com>
2023-07-06 20:08:48 -0400

    fixed invoice css

--- src/pages/sales/Sales.tsx
+++ src/pages/sales/Sales.tsx
@@ -29,44 +29,38 @@ export const Sales = () => {
   );
 
   return (
-    <div className="sales ">
+    <div className="sales common-container">
       <div className="row">
         <h1 className="title">Sales</h1>
       </div>
       <div className="row">
         <div className="date-pickers">
-          <div>
-            <span className="text-muted">Order Date Between:</span>
+          <span className="text-muted">Order Date Between:</span>
+          <DatePicker
+            enableTabLoop={false}
+            selected={new Date(initialDate.getTime() + initialDate.getTimezoneOffset() * 60000)}
+            minDate={minDate}
+            maxDate={new Date()}
+            onChange={(date) => setInitialDate(date as Date)}
+            className="form-control"
+          />
+          <DatePicker
+            enableTabLoop={false}
+            selected={new Date(finalDate.getTime() + finalDate.getTimezoneOffset() * 60000)}
+            minDate={minDate}
+            maxDate={new Date()}
+            onChange={(date) => setFinalDate(date as Date)}
+            className="form-control"
+          />
+        </div>
 
-            <DatePicker
-              enableTabLoop={false}
-              selected={new Date(initialDate.getTime() + initialDate.getTimezoneOffset() * 60000)}
-              minDate={minDate}
-              maxDate={new Date()}
-              onChange={(date) => setInitialDate(date as Date)}
-              className="form-control"
-            />
-
-            <DatePicker
-              enableTabLoop={false}
-              selected={new Date(finalDate.getTime() + finalDate.getTimezoneOffset() * 60000)}
-              minDate={minDate}
-              maxDate={new Date()}
-              onChange={(date) => setFinalDate(date as Date)}
-              className="form-control"
-            />
-          </div>
-
-          <div>
-            <InvoicesByDateBtn
-              initialDate={initialDate}
-              finalDate={finalDate}
-              onInvoicesFetched={handleInvoicesFetched}
-            />
-          </div>
-          <div>
-            <SearchInput searchQuery={searchOrderQuery} setSearchQuery={setSearchOrderQuery} />
-          </div>
+        <div>
+          <InvoicesByDateBtn
+            initialDate={initialDate}
+            finalDate={finalDate}
+            onInvoicesFetched={handleInvoicesFetched}
+          />
+          <SearchInput searchQuery={searchOrderQuery} setSearchQuery={setSearchOrderQuery} />
         </div>
       </div>
       <div className="row">
