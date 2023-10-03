88088a4159caefefc7c5ddb14d42e27542ddb0f9
Luis_Eduardo <undernet1@gmail.com>
2023-10-03 18:46:09 -0400

    Added condition to prevent error if a product already sold is deleted.

--- src/utils/inventory.helper.ts
+++ src/utils/inventory.helper.ts
@@ -8,11 +8,24 @@ export const getProductCategoryName = (productCategoryID: number, categoriesList
   return category ? category.categoryName : '';
 };
 
-export const parseProductsJSON = (productsJson: string): OrderProduct[] =>
-  JSON.parse(productsJson).map((product: any) => ({
-    productName: product.ProductName,
-    productDescription: product.ProductDescription,
-    productQuantity: product.ProductQuantity,
-    productPrice: product.ProductPrice,
-    productCategory: product.ProductCategoryName
-  }));
+export const parseProductsJSON = (productsJson: string): OrderProduct[] => {
+  if (!productsJson || productsJson.trim() === '') {
+    return [
+      {
+        productName: 'Product no longer exist',
+        productDescription: '',
+        productQuantity: 0,
+        productPrice: 0,
+        productCategory: ''
+      }
+    ];
+  } else {
+    return JSON.parse(productsJson).map((product: any) => ({
+      productName: product.ProductName,
+      productDescription: product.ProductDescription,
+      productQuantity: product.ProductQuantity,
+      productPrice: product.ProductPrice,
+      productCategory: product.ProductCategoryName
+    }));
+  }
+};
