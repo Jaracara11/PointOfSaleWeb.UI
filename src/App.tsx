05923851f99e50df7370fb59d61b7b739ec433b9
Jaracara11 <undernet1@gmail.com>
2024-03-03 09:18:40 -0400

    Changed query client location to prevent changes on stale time while re rendering

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { AppRouter } from './router/AppRouter';
-import { QueryClient, QueryCache, QueryClientProvider } from '@tanstack/react-query';
-import { handleErrorResponse } from './services/error.service';
-
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { gcTime: 43200000, staleTime: 21600000 } },
-  queryCache: new QueryCache({
-    onError: (error: Error, query: any) => {
-      query.state.error && handleErrorResponse(error, query.state.error.message);
-    }
-  })
-});
 
 export const App = () => {
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
-        <AppRouter />
-      </QueryClientProvider>
+      <AppRouter />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import React from 'react';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { handleErrorResponse } from './services/error.service';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { gcTime: 600000, staleTime: 300000, retry: 3 } },
+  queryCache: new QueryCache({
+    onError: (error: Error, query: any) => {
+      query.state.error && handleErrorResponse(error, query.state.error.message);
+    }
+  })
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
