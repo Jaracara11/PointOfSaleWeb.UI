c8e3611cec8288ffa02650c85283665503793371
Jaracara11 <undernet1@gmail.com>
2024-03-15 09:09:57 -0400

    .

--- src/repository/categoryRepository.ts
+++ src/repository/categoryRepository.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Category } from '../interfaces/inventory/Category';
 import { userAuthorizationHeaders } from '../services/user.service';
 
@@ -5,11 +6,10 @@ const API_URL = import.meta.env.VITE_API_URL + '/categories';
 
 export const getAllCategories = async (): Promise<Category[]> => {
   try {
-    const response = await fetch(API_URL, {
+    const response = await axios.get(API_URL, {
       headers: userAuthorizationHeaders()
     });
-
-    return response.json();
+    return response.data as Category[];
   } catch (error: any) {
     return Promise.reject(error);
   }
@@ -17,13 +17,10 @@ export const getAllCategories = async (): Promise<Category[]> => {
 
 export const addCategory = async (newCategory: Category): Promise<Category> => {
   try {
-    const response = await fetch(API_URL, {
-      method: 'POST',
-      headers: userAuthorizationHeaders(),
-      body: JSON.stringify(newCategory)
+    const response = await axios.post(API_URL, newCategory, {
+      headers: userAuthorizationHeaders()
     });
-
-    return response.json();
+    return response.data as Category;
   } catch (error: any) {
     return Promise.reject(error);
   }
@@ -31,13 +28,10 @@ export const addCategory = async (newCategory: Category): Promise<Category> => {
 
 export const updateCategory = async (category: Category): Promise<Category> => {
   try {
-    const response = await fetch(`${API_URL}/edit`, {
-      method: 'PUT',
-      headers: userAuthorizationHeaders(),
-      body: JSON.stringify(category)
+    const response = await axios.put(`${API_URL}/edit`, category, {
+      headers: userAuthorizationHeaders()
     });
-
-    return response.json();
+    return response.data as Category;
   } catch (error: any) {
     return Promise.reject(error);
   }
@@ -45,8 +39,7 @@ export const updateCategory = async (category: Category): Promise<Category> => {
 
 export const deleteCategory = async (categoryID: number): Promise<void> => {
   try {
-    await fetch(`${API_URL}/${categoryID}/delete`, {
-      method: 'DELETE',
+    await axios.delete(`${API_URL}/${categoryID}/delete`, {
       headers: userAuthorizationHeaders()
     });
   } catch (error: any) {
