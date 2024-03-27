421941014642b6dd27fd97b12e26d81130a30262
Luis Eduardo <Luis.Eduardo@amtrustgroup.com>
2024-03-27 13:49:13 -0400

    Updated deployment script

--- deploy_pos_ui.sh
+++ deploy_pos_ui.sh
@@ -1,14 +1,24 @@
 #!/bin/bash -x
 
+set -e
+
+# Configuration
+GIT_BRANCH="master"
+DOCKER_IMAGE_NAME="pos.ui"
+CONTAINER_NAME="pos-web.ui"
+PORT_MAPPING="3000:3000"
+
+# Deployment Steps
 cd PointOfSaleWeb.UI
-git checkout master
+git checkout "$GIT_BRANCH"
 git config pull.rebase false
 git pull
-docker stop pos-web.ui
-docker rm pos-web.ui
-docker build -t pos.ui .
-docker run -d -p 3000:3000 --restart unless-stopped --name pos-web.ui pos.ui
+docker stop "$CONTAINER_NAME" || true  # Ignore errors if container doesn't exist
+docker rm "$CONTAINER_NAME" || true    # Ignore errors if container doesn't exist
+docker build -t "$DOCKER_IMAGE_NAME" .
+docker run -d -p "$PORT_MAPPING" --restart unless-stopped --name "$CONTAINER_NAME" "$DOCKER_IMAGE_NAME"
 docker image prune -f
 
+# Logging
 echo "List of running Docker containers:"
 docker ps
