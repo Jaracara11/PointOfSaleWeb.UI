e4b87d013d00022a1ebaab2eb05e9dffd3931fbf
Luis Eduardo <Luis.Eduardo@amtrustgroup.com>
2024-03-27 15:08:33 -0400

    .

--- deploy_pos_ui.sh
+++ deploy_pos_ui.sh
@@ -13,15 +13,15 @@ cd PointOfSaleWeb.UI
 git checkout "$GIT_BRANCH"
 git config pull.rebase false
 git pull
-docker stop "$CONTAINER_NAME" || true  # Ignore errors if container doesn't exist
-docker rm "$CONTAINER_NAME" || true    # Ignore errors if container doesn't exist
-docker build -t "$DOCKER_IMAGE_NAME" .
-docker run -d -p "$PORT_MAPPING" --restart unless-stopped --name "$CONTAINER_NAME" "$DOCKER_IMAGE_NAME"
+sudo docker stop "$CONTAINER_NAME" || true  # Ignore errors if container doesn't exist
+sudo docker rm "$CONTAINER_NAME" || true    # Ignore errors if container doesn't exist
+sudo docker build -t "$DOCKER_IMAGE_NAME" .
+sudo docker run -d -p "$PORT_MAPPING" --restart unless-stopped --name "$CONTAINER_NAME" "$DOCKER_IMAGE_NAME"
 
 # Cleanup
-docker system prune -af   # Remove all stopped containers, unused networks, and dangling images
-docker volume prune -f    # Remove all unused volumes
+sudo docker system prune -af   # Remove all stopped containers, unused networks, and dangling images
+sudo docker volume prune -f    # Remove all unused volumes
 
 # Logging
 echo "List of running Docker containers:"
-docker ps
+sudo docker ps
